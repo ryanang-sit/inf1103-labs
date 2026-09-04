b62107bccb36e33593014fcb381d762a566d8a97
Ryan Ang <rryanang@gmail.com>
2026-09-04 16:48:25 +0800

    Update Profile

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -2,3 +2,12 @@ print("==========")
 print("Welcome here")
 print("My first post!")
 print("===========")
+
+username = "rryanang"
+bio = "content creator"
+followers = 45000
+
+print("USername:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
