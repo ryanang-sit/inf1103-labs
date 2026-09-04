26d2cd2fbe8ebdd9e85c3f04ed15562b798b31b5
Ryan Ang <rryanang@gmail.com>
2026-09-04 16:58:34 +0800

    Follower Growth Tracker

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,13 +1,29 @@
+#activity 1
 print("==========")
 print("Welcome here")
 print("My first post!")
 print("===========")
 
+
+#activity 2
 username = "rryanang"
 bio = "content creator"
 followers = 45000
 
-print("USername:", username)
+print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+#activity 3
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
 
