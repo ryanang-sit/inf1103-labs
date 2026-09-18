3530a0dd37f8ddc6029298e863cb72a1235393e3
Ryan Ang <rryanang@gmail.com>
2026-09-18 13:39:40 +0800

    Calculate Tax Function

--- INF1103-Labs/modular_auditor.py
+++ INF1103-Labs/modular_auditor.py
@@ -21,6 +21,10 @@ def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
 
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
 #initialize the inventory to zero in the start
 inventory = 0
 failed_entries = 0
